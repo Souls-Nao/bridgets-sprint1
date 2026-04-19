49deec590c98c340c9e537b50bac48aaf45071e9
Souls_Nao <114371410+Souls-Nao@users.noreply.github.com>
2026-04-19 12:55:41 -0600

    Update validadores.py

--- backend_sprint1/validadores.py
+++ backend_sprint1/validadores.py
@@ -2,10 +2,12 @@ from pydantic import BaseModel, EmailStr, Field
 
 class RegistroPeticion(BaseModel):
     nombre_completo: str = Field(..., min_length=3, max_length=50)
+    codigo: str = Field(..., min_length=1)
     correo_electronico: EmailStr
-    password: str = Field(..., min_length=6, description="La contraseña debe tener al menos 6 caracteres")
+    usuario_login: str = Field(..., min_length=3, max_length=20)
+    password: str = Field(..., min_length=6)
     tipo_cuenta: str = Field(..., pattern="^(estudiante|tutor)$")
 
 class LoginPeticion(BaseModel):
-    correo_electronico: EmailStr
+    usuario_login: str # Cambiado de correo a usuario
     password: str
